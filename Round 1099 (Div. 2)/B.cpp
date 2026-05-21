5100246c952f99c44e1ef348a5f79ee50a1a30f0
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2026-05-22 00:28:48 +0530

    Round 1099

---
+++ Round 1099 (Div. 2)/B.cpp
@@ -0,0 +1,127 @@
+// HARE RAM HARE KRISHNA
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const double PI = 3.14159265358979323846;
+#define INF (int)1e18
+#define f first
+#define s second
+#define pb(a) push_back(a)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define Ankur_Dwivedi()                   \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);                      \
+    }
+
+#define couty cout << "YES" << endl
+#define coutn cout << "NO" << endl
+#define cout(a) cout << a << endl
+
+#define ain(i, arr, n)          \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> arr[i];          \
+    }
+#define aout(i, arr, n)         \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << arr[i] << " ";  \
+    }
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+string binary(int x)
+{
+    string s = "";
+    for (int i = 31; i >= 0; i--)
+    {
+        int a = ((x >> i) & 1);
+        if (a == 1)
+        {
+            s += '1';
+        }
+        else
+        {
+            s += '0';
+        }
+    }
+    return s;
+}
+
+const int MAXN = 10000000;
+std::bitset<MAXN + 1> is_prime;
+
+void SieveOfEratosthenes()
+{
+    is_prime.set();
+    is_prime[0] = is_prime[1] = 0;
+
+    for (int p = 2; p * p <= MAXN; p++)
+    {
+        if (is_prime[p])
+        {
+            for (int i = p * p; i <= MAXN; i += p)
+            {
+                is_prime[i] = 0;
+            }
+        }
+    }
+}
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    ain(i,a,n);
+
+    vector<int> temp = a;
+    int ct = -1;
+    for(int i=1; i<n; i++){
+        if(temp[i] < temp[i-1]){
+            ct = max(ct, temp[i-1] - temp[i]);
+            temp[i] += ct;
+        }
+    }
+
+    if(ct == -1){
+        couty;
+        return;
+    }
+
+    temp = a;
+    for(int i=1; i<n; i++){
+        if(temp[i] < temp[i-1]){
+            temp[i] += ct;
+            if(temp[i] < temp[i-1]){
+                coutn;
+                return;
+            }
+        }
+    }
+
+    couty;
+}
+
+int32_t main()
+{
+    Ankur_Dwivedi();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
---
+++ Round 1099 (Div. 2)/C.cpp
@@ -0,0 +1,155 @@
+// HARE RAM HARE KRISHNA
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const double PI = 3.14159265358979323846;
+#define INF (int)1e18
+#define f first
+#define s second
+#define pb(a) push_back(a)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define Ankur_Dwivedi()                   \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);                      \
+    }
+
+#define couty cout << "YES" << endl
+#define coutn cout << "NO" << endl
+#define cout(a) cout << a << endl
+
+#define ain(i, arr, n)          \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> arr[i];          \
+    }
+#define aout(i, arr, n)         \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << arr[i] << " ";  \
+    }
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+string binary(int x)
+{
+    string s = "";
+    for (int i = 31; i >= 0; i--)
+    {
+        int a = ((x >> i) & 1);
+        if (a == 1)
+        {
+            s += '1';
+        }
+        else
+        {
+            s += '0';
+        }
+    }
+    return s;
+}
+
+const int MAXN = 10000000;
+std::bitset<MAXN + 1> is_prime;
+
+void SieveOfEratosthenes()
+{
+    is_prime.set();
+    is_prime[0] = is_prime[1] = 0;
+
+    for (int p = 2; p * p <= MAXN; p++)
+    {
+        if (is_prime[p])
+        {
+            for (int i = p * p; i <= MAXN; i += p)
+            {
+                is_prime[i] = 0;
+            }
+        }
+    }
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    ain(i, a, n);
+
+    vector<pair<int, int>> arr;
+    arr.reserve(n * 65);
+
+    for (int i = 0; i < n; i++)
+    {
+        int x = a[i];
+        int steps = 0;
+        while (x > 2)
+        {
+            arr.push_back({x, steps});
+            steps++;
+            if (x % 2 == 0)
+            {
+                x /= 2;
+            }
+            else
+            {
+                x++;
+            }
+        }
+        if (x == 2)
+        {
+            arr.push_back({2, steps});
+            arr.push_back({1, steps + 1});
+        }
+        else
+        {
+            arr.push_back({1, steps});
+            arr.push_back({2, steps + 1});
+        }
+    }
+
+    sort(all(arr));
+    int ans = INF;
+
+    for (int i = 0; i < arr.size();)
+    {
+        int j = i;
+        int sum = 0;
+        int ct = 0;
+        while (j < arr.size() && arr[j].first == arr[i].first)
+        {
+            ct++;
+            sum += arr[j].second;
+            j++;
+        }
+        if (ct == n)
+        {
+            ans = min(ans, sum);
+        }
+        i = j;
+    }
+
+    cout(ans);
+}
+
+int32_t main()
+{
+    Ankur_Dwivedi();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
